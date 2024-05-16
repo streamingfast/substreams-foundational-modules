9f2e0d75597924ea431ed714855bbfe9ffe83f80
YaroShkvorets <shkvorets@gmail.com>
2024-05-15 22:35:17 -0400

    regenerate protobufs

--- antelope-common/src/pb/mod.rs
+++ antelope-common/src/pb/mod.rs
@@ -1,25 +1,9 @@
 // @generated
 pub mod sf {
-    pub mod antelope {
-        pub mod r#type {
-            // @@protoc_insertion_point(attribute:sf.antelope.type.v1)
-            pub mod v1 {
-                include!("sf.antelope.type.v1.rs");
-                // @@protoc_insertion_point(sf.antelope.type.v1)
-            }
-        }
-    }
     // @@protoc_insertion_point(attribute:sf.substreams)
     pub mod substreams {
         include!("sf.substreams.rs");
         // @@protoc_insertion_point(sf.substreams)
-        pub mod antelope {
-            // @@protoc_insertion_point(attribute:sf.substreams.antelope.v1)
-            pub mod v1 {
-                include!("sf.substreams.antelope.v1.rs");
-                // @@protoc_insertion_point(sf.substreams.antelope.v1)
-            }
-        }
         pub mod index {
             // @@protoc_insertion_point(attribute:sf.substreams.index.v1)
             pub mod v1 {
