b51db7de79107565b85955b3186fb83c6270ea5c
YaroShkvorets <shkvorets@gmail.com>
2024-05-17 00:27:10 -0400

    remove unneded proto rs file

--- antelope-common/src/pb/mod.rs
+++ antelope-common/src/pb/mod.rs
@@ -11,10 +11,5 @@ pub mod sf {
                 // @@protoc_insertion_point(sf.substreams.antelope.v1)
             }
         }
-        // @@protoc_insertion_point(attribute:sf.substreams.v1)
-        pub mod v1 {
-            include!("sf.substreams.v1.rs");
-            // @@protoc_insertion_point(sf.substreams.v1)
-        }
     }
 }
