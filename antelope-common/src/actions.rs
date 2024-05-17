29f9e6c404c4cebc83489a32882e08dbda805da0
YaroShkvorets <shkvorets@gmail.com>
2024-05-17 13:49:03 -0400

    update substreams-rs

--- antelope-common/src/actions.rs
+++ antelope-common/src/actions.rs
@@ -1,16 +1,28 @@
-use substreams_antelope::pb::ActionTrace;
+use substreams_antelope::pb::{ActionTrace, PermissionLevel};
 
+// https://docs.dfuse.eosnation.io/eosio/public-apis/reference/search/terms/
 pub fn action_keys(action: &ActionTrace) -> Vec<String> {
-    let auths = action.receipt.as_ref().unwrap().auth_sequence.iter();
-    let mut keys = Vec::with_capacity(auths.len() + 3);
+    let receiver = &action.receiver;
+    let action = action.action.as_ref().unwrap();
+    let mut keys = Vec::with_capacity(action.authorization.len() * 2 + 3);
 
     keys.extend(vec![
-        format!("code:{}", action.action.as_ref().unwrap().account),
-        format!("receiver:{}", action.receiver),
-        format!("action:{}", action.action.as_ref().unwrap().name),
+        format!("code:{}", action.account),
+        format!("receiver:{}", receiver),
+        format!("action:{}", action.name),
     ]);
 
-    keys.extend(auths.map(|auth| format!("auth:{}", auth.account_name)));
+    keys.extend(
+        action
+            .authorization
+            .iter()
+            .flat_map(|PermissionLevel { actor, permission }| {
+                vec![
+                    format!("auth:{actor}@{permission}"),
+                    format!("auth:{actor}"),
+                ]
+            }),
+    );
 
     keys
 }
