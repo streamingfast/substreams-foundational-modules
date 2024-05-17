4d6779411bdc6dd42bde66e13be394e72f12d671
YaroShkvorets <shkvorets@gmail.com>
2024-05-17 17:46:22 -0400

    add data filters

--- antelope-common/src/actions.rs
+++ antelope-common/src/actions.rs
@@ -1,3 +1,4 @@
+use serde_json::Value;
 use substreams_antelope::pb::{ActionTrace, PermissionLevel};
 
 // i.e. https://docs.dfuse.eosnation.io/eosio/public-apis/reference/search/terms/
@@ -5,12 +6,29 @@ pub fn action_keys(action: &ActionTrace) -> Vec<String> {
     let receiver = &action.receiver;
     let action = action.action.as_ref().unwrap();
     let mut keys = Vec::with_capacity(action.authorization.len() * 2 + 3);
+    let json_data: Value = match serde_json::from_str(&action.json_data) {
+        Ok(data) => data,
+        Err(_) => Value::Object(Default::default()),
+    };
 
     keys.extend(vec![
         format!("code:{}", action.account),
         format!("receiver:{}", receiver),
         format!("action:{}", action.name),
     ]);
+
+    keys.extend(
+        json_data
+            .as_object()
+            .unwrap()
+            .iter()
+            .filter_map(|(key, value)| match value {
+                Value::String(value) => Some(format!("data.{}:{}", key, value)),
+                Value::Number(value) => Some(format!("data.{}:{}", key, value)),
+                Value::Bool(value) => Some(format!("data.{}:{}", key, value)),
+                _ => None,
+            }),
+    );
 
     keys.extend(
         action
