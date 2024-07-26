30c4a329fca454fcc7984e9ea3f58f767ebd5ff6
YaroShkvorets <shkvorets@gmail.com>
2024-07-25 20:18:39 -0400

    refactor DRY

--- antelope-common/src/index.rs
+++ antelope-common/src/index.rs
@@ -1,16 +1,30 @@
 use std::collections::HashSet;
 
+use serde_json::Value;
 use substreams::pb::sf::substreams::index::v1::Keys;
-use substreams_antelope::pb::ActionTraces;
-
-use crate::actions::{action_keys, action_keys_extra};
+use substreams_antelope::pb::{ActionTrace, ActionTraces, PermissionLevel};
 
 #[substreams::handlers::map]
 fn index_actions(actions: ActionTraces) -> Result<Keys, substreams::errors::Error> {
+    collect_keys(actions, action_keys)
+}
+
+#[substreams::handlers::map]
+fn index_actions_extra(actions: ActionTraces) -> Result<Keys, substreams::errors::Error> {
+    collect_keys(actions, action_keys_extra)
+}
+
+fn collect_keys<F>(
+    actions: ActionTraces,
+    key_extractor: F,
+) -> Result<Keys, substreams::errors::Error>
+where
+    F: Fn(&ActionTrace) -> Vec<String>,
+{
     let keys = actions
         .action_traces
-        .into_iter()
-        .flat_map(|action| action_keys(&action))
+        .iter()
+        .flat_map(key_extractor)
         .collect::<HashSet<_>>()
         .into_iter()
         .collect();
@@ -18,15 +32,64 @@ fn index_actions(actions: ActionTraces) -> Result<Keys, substreams::errors::Erro
     Ok(Keys { keys })
 }
 
-#[substreams::handlers::map]
-fn index_actions_extra(actions: ActionTraces) -> Result<Keys, substreams::errors::Error> {
-    let keys = actions
-        .action_traces
-        .into_iter()
-        .flat_map(|action| action_keys_extra(&action))
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect();
+// i.e. https://docs.dfuse.eosnation.io/eosio/public-apis/reference/search/terms/
+pub fn action_keys_extra(trace: &ActionTrace) -> Vec<String> {
+    let action = trace.action.as_ref().unwrap();
+    if action.account == "eosio" && action.name == "onblock" {
+        return vec![];
+    }
+    let mut keys = Vec::with_capacity(action.authorization.len() * 2 + 5 + 3);
+    let json_data: Value = match serde_json::from_str(&action.json_data) {
+        Ok(data) => data,
+        Err(_) => Value::Object(Default::default()),
+    };
 
-    Ok(Keys { keys })
+    keys.extend(vec![
+        format!("code:{}", action.account),
+        format!("receiver:{}", trace.receiver),
+        format!("action:{}", action.name),
+    ]);
+
+    keys.extend(
+        json_data
+            .as_object()
+            .unwrap()
+            .iter()
+            .filter_map(|(key, value)| match value {
+                Value::String(value) => Some(format!("data.{}:{}", key, value)),
+                Value::Number(value) => Some(format!("data.{}:{}", key, value)),
+                Value::Bool(value) => Some(format!("data.{}:{}", key, value)),
+                _ => None,
+            }),
+    );
+
+    keys.extend(
+        action
+            .authorization
+            .iter()
+            .flat_map(|PermissionLevel { actor, permission }| {
+                vec![
+                    format!("auth:{actor}@{permission}"),
+                    format!("auth:{actor}"),
+                ]
+            }),
+    );
+
+    if trace.creator_action_ordinal == 0 {
+        keys.push("input:true".to_string());
+    }
+
+    keys
+}
+
+pub fn action_keys(trace: &ActionTrace) -> Vec<String> {
+    let action = trace.action.as_ref().unwrap();
+    if action.account == "eosio" && action.name == "onblock" {
+        return vec![];
+    }
+    vec![
+        format!("code:{}", action.account),
+        format!("receiver:{}", trace.receiver),
+        format!("action:{}", action.name),
+    ]
 }
--- antelope-common/src/maps.rs
+++ antelope-common/src/maps.rs
@@ -1,21 +1,22 @@
 use std::collections::HashSet;
 
-use crate::actions::{action_keys, action_keys_extra};
-use substreams::matches_keys_in_parsed_expr;
+use substreams::{errors::Error, matches_keys_in_parsed_expr};
 use substreams_antelope::{
-    pb::{ActionTraces, TransactionTraces},
+    pb::{ActionTrace, ActionTraces, TransactionTraces},
     Block,
 };
 
+use crate::index::{action_keys, action_keys_extra};
+
 #[substreams::handlers::map]
-fn all_transactions(block: Block) -> Result<TransactionTraces, substreams::errors::Error> {
+fn all_transactions(block: Block) -> Result<TransactionTraces, Error> {
     Ok(TransactionTraces {
         transaction_traces: block.into_transaction_traces().collect(),
     })
 }
 
 #[substreams::handlers::map]
-fn all_actions(transactions: TransactionTraces) -> Result<ActionTraces, substreams::errors::Error> {
+fn all_actions(transactions: TransactionTraces) -> Result<ActionTraces, Error> {
     let action_traces = transactions
         .transaction_traces
         .into_iter()
@@ -26,59 +27,44 @@ fn all_actions(transactions: TransactionTraces) -> Result<ActionTraces, substrea
 }
 
 #[substreams::handlers::map]
-fn filtered_actions(
-    query: String,
-    actions: ActionTraces,
-) -> Result<ActionTraces, substreams::errors::Error> {
-    let action_traces = actions
-        .action_traces
-        .into_iter()
-        .filter(|action| {
-            let keys = action_keys(action);
+fn filtered_actions(query: String, actions: ActionTraces) -> Result<ActionTraces, Error> {
+    filter_actions(query, actions, action_keys)
+}
 
-            // will panic if the query is invalid
-            matches_keys_in_parsed_expr(&keys, &query).unwrap()
-        })
-        .collect();
-
-    Ok(ActionTraces { action_traces })
+#[substreams::handlers::map]
+fn filtered_actions_extra(query: String, actions: ActionTraces) -> Result<ActionTraces, Error> {
+    filter_actions(query, actions, action_keys_extra)
 }
 
 #[substreams::handlers::map]
 fn filtered_transactions(
     query: String,
     transactions: TransactionTraces,
-) -> Result<TransactionTraces, substreams::errors::Error> {
-    let transaction_traces = transactions
-        .transaction_traces
-        .into_iter()
-        .filter(|trx| {
-            let keys = trx
-                .action_traces
-                .iter()
-                .flat_map(action_keys)
-                .collect::<HashSet<_>>()
-                .into_iter()
-                .collect::<Vec<_>>();
-
-            // will panic if the query is invalid
-            matches_keys_in_parsed_expr(&keys, &query).unwrap()
-        })
-        .collect();
-
-    Ok(TransactionTraces { transaction_traces })
+) -> Result<TransactionTraces, Error> {
+    filter_transactions(query, transactions, action_keys)
 }
 
 #[substreams::handlers::map]
-fn filtered_actions_extra(
+fn filtered_transactions_extra(
+    query: String,
+    transactions: TransactionTraces,
+) -> Result<TransactionTraces, Error> {
+    filter_transactions(query, transactions, action_keys_extra)
+}
+
+fn filter_actions<F>(
     query: String,
     actions: ActionTraces,
-) -> Result<ActionTraces, substreams::errors::Error> {
+    key_extractor: F,
+) -> Result<ActionTraces, Error>
+where
+    F: Fn(&ActionTrace) -> Vec<String>,
+{
     let action_traces = actions
         .action_traces
         .into_iter()
         .filter(|action| {
-            let keys = action_keys_extra(action);
+            let keys = key_extractor(action);
 
             // will panic if the query is invalid
             matches_keys_in_parsed_expr(&keys, &query).unwrap()
@@ -88,11 +74,14 @@ fn filtered_actions_extra(
     Ok(ActionTraces { action_traces })
 }
 
-#[substreams::handlers::map]
-fn filtered_transactions_extra(
+fn filter_transactions<F>(
     query: String,
     transactions: TransactionTraces,
-) -> Result<TransactionTraces, substreams::errors::Error> {
+    key_extractor: F,
+) -> Result<TransactionTraces, Error>
+where
+    F: Fn(&ActionTrace) -> Vec<String>,
+{
     let transaction_traces = transactions
         .transaction_traces
         .into_iter()
@@ -100,7 +89,7 @@ fn filtered_transactions_extra(
             let keys = trx
                 .action_traces
                 .iter()
-                .flat_map(action_keys_extra)
+                .flat_map(&key_extractor)
                 .collect::<HashSet<_>>()
                 .into_iter()
                 .collect::<Vec<_>>();
