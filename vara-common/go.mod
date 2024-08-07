446374196d9754a01b5a837fa0bd1fe278258fb0
Charles Billette <charles@cbillette.com>
2024-08-07 15:04:38 -0400

    work on 100 block.

--- vara-common/go.mod
+++ vara-common/go.mod
@@ -2,7 +2,7 @@ module github.com/streamingfast/substreams-foundational-modules/vara-common
 
 go 1.22.0
 
-replace github.com/centrifuge/go-substrate-rpc-client/v4 => github.com/streamingfast/go-substrate-rpc-client/v4 v4.0.0-20240801195649-1435d6f2534e
+replace github.com/centrifuge/go-substrate-rpc-client/v4 => github.com/streamingfast/go-substrate-rpc-client/v4 v4.0.0-20240802141237-cb3f49e2ebd1
 
 require (
 	github.com/centrifuge/go-substrate-rpc-client/v4 v4.2.1
@@ -11,7 +11,12 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/cors v1.10.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
@@ -24,7 +29,6 @@ require (
 
 require (
 	github.com/ChainSafe/go-schnorrkel v1.0.0 // indirect
-	github.com/centrifuge/go-substrate-rpc-client v1.1.0
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/decred/base58 v1.0.4 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
--- vara-common/main_test.go
+++ vara-common/main_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
+	"path"
+	"strings"
 	"testing"
 
 	pbgear "github.com/streamingfast/substreams-foundational-modules/vara-common/pb/sf/gear/type/v1"
@@ -18,6 +22,7 @@ func Test_MapDecodedBlock_EmptyBlock(t *testing.T) {
 }
 
 func Test_MapDecodedBlock(t *testing.T) {
+
 	blk := &pbgear.Block{}
 	b, err := os.ReadFile("testdata/block_14463900.json")
 	require.NoError(t, err)
@@ -26,5 +31,49 @@ func Test_MapDecodedBlock(t *testing.T) {
 
 	dblock, err := map_decoded_block(blk)
 	require.NoError(t, err)
-	fmt.Println(dblock.Number)
+
+	j, err := json.MarshalIndent(dblock, "", "  ")
+	require.NoError(t, err)
+	fmt.Println(string(j))
+}
+
+func Test_MapDecodedBlocks(t *testing.T) {
+
+	dataDir := "./testdata"
+	files, err := os.ReadDir(dataDir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, file := range files {
+		//check for extension .blocks.jsonl
+		if strings.HasSuffix(file.Name(), ".blocks.jsonl") {
+
+			fmt.Println("processing:", file.Name())
+			//open file
+			f, err := os.Open(path.Join(dataDir, file.Name()))
+			require.NoError(t, err)
+
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				// Unmarshal each line into &pbgear.Block{}
+				block := &pbgear.Block{}
+				err = json.Unmarshal([]byte(scanner.Text()), block)
+				require.NoError(t, err)
+
+				dblock, err := map_decoded_block(block)
+				require.NoError(t, err)
+				fmt.Println("decoded block", dblock.Number, hex.EncodeToString(block.Hash))
+				j, err := json.MarshalIndent(dblock, "", "  ")
+				require.NoError(t, err)
+				fmt.Println(string(j))
+			}
+
+			//closing the file
+			if err := f.Close(); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
 }
