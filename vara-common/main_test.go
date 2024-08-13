388f8a9e6523c9becbb2824575037ceaf9bc9728
Charles Billette <charles@cbillette.com>
2024-08-13 09:19:18 -0400

    better test and local.go

--- vara-common/main_test.go
+++ vara-common/main_test.go
@@ -23,57 +23,58 @@ func Test_MapDecodedBlock_EmptyBlock(t *testing.T) {
 }
 
 func Test_MapDecodedBlock(t *testing.T) {
-	tests := []struct {
-		name string
-		key  string
-	}{
-		{name: "spec100", key: "100"},
-		{name: "spec120", key: "17600"},
-		{name: "spec130", key: "138000"},
-		{name: "spec140", key: "1380000"},
-		{name: "spec210", key: "4182200"},
-		{name: "spec310", key: "4433500"},
-		{name: "spec320", key: "4641600"},
-		{name: "spec330", key: "4961300"},
-		{name: "spec340", key: "4986900"},
-		{name: "spec350", key: "5046400"},
-		{name: "spec1000", key: "5079000"},
-		{name: "spec1010", key: "6760600"},
-		{name: "spec1020", key: "7196000"},
-		{name: "spec1030", key: "8384900"},
-		{name: "spec1040", key: "8754200"},
-		{name: "spec1050", key: "8775700"},
-		{name: "spec1110", key: "9876600"},
-		{name: "spec1200", key: "10849200"},
-		{name: "spec1210", key: "10934500"},
-		{name: "spec1300", key: "11627100"},
-		{name: "spec1310", key: "12224800"},
-		{name: "spec1400", key: "12629600"},
-		{name: "spec1410", key: "12885100"},
-		{name: "spec1420", key: "14463900"},
+	//tests := []struct {
+	//	name string
+	//	key  string
+	//}{
+	//	{name: "spec100", key: "100"},
+	//	{name: "spec120", key: "17600"},
+	//	{name: "spec130", key: "138000"},
+	//	{name: "spec140", key: "1380000"},
+	//	{name: "spec210", key: "4182200"},
+	//	{name: "spec310", key: "4433500"},
+	//	{name: "spec320", key: "4641600"},
+	//	{name: "spec330", key: "4961300"},
+	//	{name: "spec340", key: "4986900"},
+	//	{name: "spec350", key: "5046400"},
+	//	{name: "spec1000", key: "5079000"},
+	//	{name: "spec1010", key: "6760600"},
+	//	{name: "spec1020", key: "7196000"},
+	//	{name: "spec1030", key: "8384900"},
+	//	{name: "spec1040", key: "8754200"},
+	//	{name: "spec1050", key: "8775700"},
+	//	{name: "spec1110", key: "9876600"},
+	//	{name: "spec1200", key: "10849200"},
+	//	{name: "spec1210", key: "10934500"},
+	//	{name: "spec1300", key: "11627100"},
+	//	{name: "spec1310", key: "12224800"},
+	//	{name: "spec1400", key: "12629600"},
+	//	{name: "spec1410", key: "12885100"},
+	//	{name: "spec1420", key: "14463900"},
+	//}
+
+	//for _, tt := range tests {
+	//	t.Run(tt.name, func(t *testing.T) {
+	//		key := tt.key
+	for _, blocks := range testBlocks {
+		for _, b := range blocks {
+			dblock, err := MapDecodedBlock(b)
+			require.NoError(t, err)
+			require.NotNil(t, dblock)
+
+			fmt.Println(dblock.Number)
+			extrinsics, err := FilteredExtrinsics("extrinsic:Gear.run:event:System.ExtrinsicSuccess", dblock)
+			require.NoError(t, err)
+			require.Equal(t, 1, len(extrinsics.Extrinsics))
+
+			//j, err := json.MarshalIndent(dblock, "", "  ")
+			//require.NoError(t, err)
+			//fmt.Println(string(j))
+		}
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			key := tt.key
-			blocks, ok := testBlocks[key]
-			require.True(t, ok)
-
-			for _, b := range blocks {
-				dblock, err := MapDecodedBlock(b)
-				require.NoError(t, err)
-				require.NotNil(t, dblock)
-
-				extrinsics, err := FilteredExtrinsics("extrinsic:Gear.run:event:System.ExtrinsicSuccess", dblock)
-				require.NoError(t, err)
-				require.Equal(t, 1, len(extrinsics.Extrinsics))
-
-				//j, err := json.MarshalIndent(dblock, "", "  ")
-				//require.NoError(t, err)
-				//fmt.Println(string(j))
-			}
-		})
-	}
+	//})
+	//}
 }
 
 func loadBlocks() map[string][]*pbgear.Block {
