a30f854fc91170cb802191d6b6351ed31ec8f73e
Ulysse Corbeil <ulysse98@hotmail.com>
2025-09-17 17:23:31 -0400

    pb gen

---
+++ solana/common/src/pb/mod.rs
@@ -0,0 +1,15 @@
+// @generated
+pub mod sf {
+    // @@protoc_insertion_point(attribute:sf.substreams)
+    pub mod substreams {
+        include!("sf.substreams.rs");
+        // @@protoc_insertion_point(sf.substreams)
+        pub mod solana {
+            // @@protoc_insertion_point(attribute:sf.substreams.solana.v1)
+            pub mod v1 {
+                include!("sf.substreams.solana.v1.rs");
+                // @@protoc_insertion_point(sf.substreams.solana.v1)
+            }
+        }
+    }
+}
